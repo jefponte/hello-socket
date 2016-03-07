e57178a6045ba7c42d180381f4e97d3f321c32f6
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2016-03-07 13:02:33 -0300

    Em outra funcao para que fique fora da tela

--- java/servidor/src/ola_socket/teste/Main.java
+++ java/servidor/src/ola_socket/teste/Main.java
@@ -18,33 +18,8 @@ public class Main {
 				final Socket conexao = servidor.accept();	
 				System.out.println("Nova Conexao!");
 				//Para que possamos ter várias conexões ao mesmo tempo 
-				//Criamos uma Thread para cada conexão. 
-				//
-				Thread processando = new Thread(new Runnable() {
-					
-					@Override
-					public void run() {
-						//Aqui eu pego a conexao e faco alguma troca interessante. 
-						//Esperamos que o cliente envie uma string. Vamos devolver outra e fechar a conexao. 
-						try {
-							PrintStream saida = new PrintStream(conexao.getOutputStream());
-							BufferedReader entrada = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
-							String mensagemDoCliente = entrada.readLine();
-							System.out.println("Primeira Mensagem do Cliente: "+mensagemDoCliente);
-							saida.println("Eu sou o servidor e recebi sua mensagem, agora vou fechar a conexao. Adeus!");
-							conexao.close();
-							
-						} catch (IOException e) {
-							
-							e.printStackTrace();
-						}
-		                
-
-					}
-				});
-				processando.start();
 				//Independentemente da Thread o laco continua rodando. 
-				
+				processandoConexao(conexao);
 			}while(true);
 			
 			
@@ -52,7 +27,33 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 
 	}
+	public static void processandoConexao(final Socket conexao){
+		Thread processando = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				//Aqui eu pego a conexao e faco alguma troca interessante. 
+				//Esperamos que o cliente envie uma string. Vamos devolver outra e fechar a conexao. 
+				try {
+					PrintStream saida = new PrintStream(conexao.getOutputStream());
+					BufferedReader entrada = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+					String mensagemDoCliente = entrada.readLine();
+					System.out.println("Primeira Mensagem do Cliente: "+mensagemDoCliente);
+					saida.println("Eu sou o servidor e recebi sua mensagem, agora vou fechar a conexao. Adeus!");
+					conexao.close();
+					
+				} catch (IOException e) {
+					
+					e.printStackTrace();
+				}
+                
 
+			}
+		});
+		processando.start();
+		
+	}
 }
